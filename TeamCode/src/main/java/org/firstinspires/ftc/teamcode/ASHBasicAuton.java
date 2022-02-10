974340bc4073490192323f3b8bec217b33d1dfd3
admiralwaffle4 <admiralzacbar2@gmail.com>
2022-02-10 15:30:41 -0500

    added an auton

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ASHBasicAuton.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class ASHBasicAuton extends LinearOpMode {
+    ASHardware ASH = new ASHardware();
+    DcMotor wheels[] = new DcMotor[3];
+}
