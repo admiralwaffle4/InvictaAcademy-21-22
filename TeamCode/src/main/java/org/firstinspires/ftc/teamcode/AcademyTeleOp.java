ff0199113dd6b336f7250dd80547f26e6b291f96
admiralwaffle4 <56054320+admiralwaffle4@users.noreply.github.com>
2022-02-19 18:20:53 -0500

    redo mason house changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AcademyTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AcademyTeleOp.java
@@ -4,46 +4,52 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="IDK Tele Op",group="Invicta Academy")
 public class AcademyTeleOp extends LinearOpMode {
+    //time and robot variables
+    private ElapsedTime runtime = new ElapsedTime();
     ASHardware robot = new ASHardware();
+
+    //setting  everything up
     DcMotor right,left,middle;
     float turn,drive,strafe;
     double leftPower,rightPower,middlePower;
 
     @Override
     public void runOpMode() {
+        //getting robot data
         robot.init(hardwareMap);
         right = robot.Right;
         left = robot.Left;
         middle = robot.Middle;
 
+        //wait for the driver to press the play button
         waitForStart();
 
         while (opModeIsActive()) {
+            //get the values from gamepad
             drive = gamepad1.left_stick_y;
             strafe = gamepad1.left_stick_x;
-            turn = -gamepad1.right_stick_x;
+            turn = gamepad1.right_stick_x;
 
-            leftPower = drive - strafe + turn;
-            rightPower = drive + strafe + turn;
-            middlePower = strafe;
+            //make sure the values aren't outside of the range
+            leftPower = Range.clip(drive + strafe - turn, -1.0, 1.0) ;
+            rightPower = Range.clip(drive - strafe + turn, -1.0, 1.0) ;
+            middlePower = Range.clip(strafe, -1.0, 1.0);
 
-            double max;
-            max = Math.max(Math.abs(leftPower), max);
-            max = Math.max(Math.abs(rightPower), max);
-            max = Math.max(Math.abs(middlePower), max);
+            //power the motors
+            left.setPower(leftPower);
+            right.setPower(rightPower);
+            middle.setPower(middlePower);
 
-            if (max > 1) {
-                leftPower /= max;
-                rightPower /= max;
-                middlePower /= max;
-            }
-
-            left.setPower(leftPower * .5);
-            right.setPower(rightPower * .5);
-            middle.setPower(middlePower * .5);
+            //give the telemetry the data
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Inputs", "drive (%.2f), strafe (%.2f), turn (%.2f)", drive, strafe, turn);
+            telemetry.addData("Motors", "left (%.2f), right (%.2f), middle (%.2f)", leftPower, rightPower, middlePower);
+            telemetry.update();
 
         }
     }
